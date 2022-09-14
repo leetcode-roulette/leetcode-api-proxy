19ee5ea79bf4b97ab92475966c55175397534c9d
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-09-14 12:28:03 -0400

    Added new button styles

--- src/components/Util/Button/Button.tsx
+++ src/components/Util/Button/Button.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useEffect, useState, PropsWithChildren } from "react";
+import React, { FC, PropsWithChildren } from "react";
 import "./styles/button.css";
 
 interface ButtonOptions extends PropsWithChildren {
 	onClick?: () => void;
-	size?: string;
+	size?: "btn-sm" | "btn-md" | "btn-lrg";
 	style?: string;
 	text?: string;
 	type?: "button" | "submit" | "reset" | undefined;
@@ -19,20 +19,11 @@ const SIZES = {
 	large: "btn-lrg",
 };
 
-const BUTTON_STYLES = {};
-
 const Button: FC<ButtonOptions> = ({ children, onClick, size, style, text, type }) => {
 	const btnSize = size ? size : SIZES["medium"];
 	const btnStyle = style ? style : STYLES["default"];
 
 	const STYLE = [btnSize, btnStyle];
-
-	function queryStyles(style: string) {
-		const styles = style.split(" ");
-
-		let x = [];
-		styles.map((s) => {});
-	}
 
 	return (
 		<button className={`btn ${STYLE.join(" ")}`} onClick={onClick} type={type}>
