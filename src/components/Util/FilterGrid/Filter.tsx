05cc1cb221c6372dda44b21d10f4b3ecc914a7e3
Eric <eric.hicks.8989@gmail.com>
2022-09-27 11:29:14 -0700

    Add code to simulate filter grid

--- src/components/Util/FilterGrid/Filter.tsx
+++ src/components/Util/FilterGrid/Filter.tsx
@@ -4,88 +4,23 @@ export interface Filter {
 	toggled?: boolean;
 }
 
-export const DEFAULT_ROW_FILTERS: Filter[] = [
-	{
-		id: 0,
-		text: "Easy",
-		toggled: true
-	},
-	{
-		id: 1,
-		text: "Medium",
-		toggled: true
-	},
-	{
-		id: 2,
-		text: "Hard",
-		toggled: true
-	},
-	{
-		id: 3,
-		text: "Premium",
-		toggled: false
-	}
-]
+export const DEFAULT_FILTERS = (() : Filter[] => {
+	const filterTextArray : string[] = [
+		"Array", "String", "Hash Table", 
+		"Dynamic Programming", "Sorting", 
+		"Math", "Greedy", "Binary Search",
+		"Heaps"
+	];
 
-export const DEFAULT_FILTERS: Filter[] = [
-	{
-		id: 0,
-		text: "Array",
-		toggled: false,
-	},
-	{
-		id: 1,
-		text: "String",
-		toggled: false,
-	},
-	{
-		id: 2,
-		text: "Hash Table",
-		toggled: false,
-	},
-	{
-		id: 3,
-		text: "Dynamic Programming",
-		toggled: false,
-	},
-	{
-		id: 4,
-		text: "Sorting",
-		toggled: false,
-	},
-	{
-		id: 5,
-		text: "Math",
-		toggled: false,
-	},
-	{
-		id: 6,
-		text: "Greedy",
-		toggled: false,
-	},
-	{
-		id: 7,
-		text: "Binary Search",
-		toggled: false,
-	},
-	{
-		id: 8,
-		text: "Heaps",
-		toggled: false,
-	},
-	{
-		id: 9,
-		text: "Greedy",
-		toggled: false,
-	},
-	{
-		id: 10,
-		text: "Binary Search",
-		toggled: false,
-	},
-	{
-		id: 11,
-		text: "Heaps",
-		toggled: false,
-	},
-];
+	const filters : Filter[] = [];
+
+	for (let i = 0; i < 71; i++) {
+		filters[i] = {
+			id: i,
+			text: filterTextArray[Math.floor(Math.random() * filterTextArray.length)],
+			toggled: false
+		};
+	}
+
+	return filters;
+})();
