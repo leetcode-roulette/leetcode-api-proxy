90ca88742a7a5d0467ba9cb6acdf2949cc961884
Eric <eric.hicks.8989@gmail.com>
2022-09-27 11:58:34 -0700

    Remove search button

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -6,22 +6,21 @@ import "./styles/content.css";
 const Content: FC = () => {
 	return (
 		<div className="content px-4">
-			<div className="mx-auto pt-4 text-center">
+			<div className="mx-auto pt-3 text-center">
 				<div className="col align-self-center">
 					<h5 className="logo py-4 fw-normal">
 						Welcome to LeetCode<span>Roulette</span>
 					</h5>
 					<div className="row justify-content-center">
-						<p className="w-75">
+						<p className="w-75 pb-1">
 							Leetcode Roulette is a service that allows users to get a random question from <a className="leetcode" href="https://leetcode.com">leetcode.com</a>, 
 							similar to a roulette game. Users are able to apply filters and 
 							search to find questions. Try it out below!
 						</p>
 					</div>
 					<FilterGrid></FilterGrid>
-					<div className="flex-row mt-5">
-						<Button>Search</Button>
-						<Button styles="btn-primary-solid">Random</Button>
+					<div className="my-5">
+						<Button size="btn-lrg" styles="btn-primary-solid">Random</Button>
 					</div>
 				</div>
 			</div>
