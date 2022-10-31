f6133187b8c49c2446fa75ba81db899b421e485f
Eric <eric.hicks.8989@gmail.com>
2022-10-31 16:07:13 -0700

    Fix: Small cleanup changes in `FilterGrid.tsx`.
    * Create variables for the `numberOfFilters` and `showMoreButton`.
    * Add `JSX.Element[]` type to `FILTER_BUTTONS` variable.

--- src/components/Util/FilterGrid/FilterGrid.tsx
+++ src/components/Util/FilterGrid/FilterGrid.tsx
@@ -31,7 +31,7 @@ class FilterGrid extends Component<FilterGridProps, FilterGridState> {
 
 	render() {
 		const DEFAULT_NUMBER_OF_FILTERS = 39;
-		const FILTER_BUTTONS = this.props.tags.map((filter: Filter) => (
+		const FILTER_BUTTONS: JSX.Element[] = this.props.tags.map((filter: Filter) => (
 			<FilterToggle
 				styles="toggle toggle-white-outline"
 				filter={filter}
@@ -42,22 +42,27 @@ class FilterGrid extends Component<FilterGridProps, FilterGridState> {
 			</FilterToggle>
 		));
 
+		const numberOfFilters: number = this.state.showMore ? FILTER_BUTTONS.length : DEFAULT_NUMBER_OF_FILTERS;
+		const showMoreButton: JSX.Element = (
+			<button className="text-white btn mt-2" onClick={this.onClick}>
+				Show {this.state.showMore ? "less" : "more"}
+			</button>
+		)
+
 		return (
 			<div className="container-fluid pt-4">
 				<div className="col">
 					<div className="d-flex flex-wrap justify-content-center">
 						{FILTER_BUTTONS.map(
 							(filterToggle, index) =>
-								index < (this.state.showMore ? FILTER_BUTTONS.length : DEFAULT_NUMBER_OF_FILTERS) && (
+								index < numberOfFilters && (
 									<div key={filterToggle.props.filter.id} className="mx-2 my-2">
 										{filterToggle}
 									</div>
 								)
 						)}
 						<div className="mx-0 my-0">
-							<button className="text-white btn mt-2" onClick={this.onClick}>
-								Show {this.state.showMore ? "less" : "more"}
-							</button>
+							{ showMoreButton }
 						</div>
 					</div>
 					
