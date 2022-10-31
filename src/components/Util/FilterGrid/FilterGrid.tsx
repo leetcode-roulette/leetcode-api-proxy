e34a20e50b3a01aa383b66c524a2620390e09ea6
Eric <eric.hicks.8989@gmail.com>
2022-10-31 16:02:13 -0700

    Fix: Change `ShowMore` buttons css.
    * Change `FilterGrid` to a class component.
    * Change the css for the `ShowMore` button.

--- src/components/Util/FilterGrid/FilterGrid.tsx
+++ src/components/Util/FilterGrid/FilterGrid.tsx
@@ -1,55 +1,70 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { Component } from "react";
 import FilterToggle from "./FilterToggle";
 import { Filter, filterToggleHandler } from "./Filter";
 
-interface FilterGridProps {
+type FilterGridProps = {
 	updateFilters: (filter: Filter) => void;
 	tags: Array<Filter>;
 }
 
-const FilterGrid: FC<FilterGridProps> = ({ updateFilters, tags }: FilterGridProps) => {
-	const [showMore, setShowMore] = useState<boolean>(false);
-	const [numberOfItems, setNumberOfItems] = useState<number>(20);
-	const FILTER_BUTTONS = tags.map((filter: Filter) => (
-		<FilterToggle
-			styles="toggle toggle-white-outline"
-			filter={filter}
-			toggled={filter.toggled}
-			onClick={() => filterToggleHandler(updateFilters, filter)}
-		>
-			{filter.text}
-		</FilterToggle>
-	));
+type FilterGridState = {
+	showMore: boolean;
+}
 
-	useEffect(() => {
-		if (showMore) {
-			setNumberOfItems(FILTER_BUTTONS.length);
-		} else {
-			setNumberOfItems(20);
-		}
-	}, [showMore]);
+class FilterGrid extends Component<FilterGridProps, FilterGridState> {
+	state: FilterGridState = {
+		showMore: false
+	}
 
-	return (
-		<div className="container-fluid pt-4">
-			<div className="col">
-				<div className="d-flex flex-wrap justify-content-center">
-					{FILTER_BUTTONS.map(
-						(filterToggle, index) =>
-							index < numberOfItems && (
-								<div key={filterToggle.props.filter.id} className="mx-2 my-2">
-									{filterToggle}
-								</div>
-							)
-					)}
-					<div className="mx-2 my-2">
-						<button className="toggle toggle-white-outline" onClick={() => setShowMore(!showMore)}>
-							Show {(showMore && "less") || "more"}
-						</button>
+	constructor(props: FilterGridProps) {
+		super(props);
+
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onClick() {
+		this.setState(state => ({
+			...state,
+			showMore: !state.showMore
+		}));
+	}
+
+	render() {
+		const DEFAULT_NUMBER_OF_FILTERS = 39;
+		const FILTER_BUTTONS = this.props.tags.map((filter: Filter) => (
+			<FilterToggle
+				styles="toggle toggle-white-outline"
+				filter={filter}
+				toggled={filter.toggled}
+				onClick={() => filterToggleHandler(this.props.updateFilters, filter)}
+			>
+				{filter.text}
+			</FilterToggle>
+		));
+
+		return (
+			<div className="container-fluid pt-4">
+				<div className="col">
+					<div className="d-flex flex-wrap justify-content-center">
+						{FILTER_BUTTONS.map(
+							(filterToggle, index) =>
+								index < (this.state.showMore ? FILTER_BUTTONS.length : DEFAULT_NUMBER_OF_FILTERS) && (
+									<div key={filterToggle.props.filter.id} className="mx-2 my-2">
+										{filterToggle}
+									</div>
+								)
+						)}
+						<div className="mx-0 my-0">
+							<button className="text-white btn mt-2" onClick={this.onClick}>
+								Show {this.state.showMore ? "less" : "more"}
+							</button>
+						</div>
 					</div>
+					
 				</div>
 			</div>
-		</div>
-	);
-};
+		)
+	}
+}
 
 export default FilterGrid;
