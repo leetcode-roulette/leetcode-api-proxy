8a2f786bdc7b8cf5b07bebd745eb30f8e7534e10
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-11-02 02:29:45 -0400

    Improved ProblemModal

--- src/components/ProblemModal/ProblemModal.tsx
+++ src/components/ProblemModal/ProblemModal.tsx
@@ -1,20 +1,70 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
-import { useModalContext } from "../../context/ModalProvider";
+import { useFilterContext, useModalContext } from "../../context";
+import { api, Problem } from "../../api";
+import Roulette from "@leetcoderoulette/roulette";
 
 import "./styles/problem-modal.css";
 
 const ProblemModal: FC = () => {
 	const [open, toggleModalOpen] = useModalContext();
+	const [filters] = useFilterContext();
+	const [problems, setProblems] = useState<Problem[]>([]);
+	const [currentProblem, setCurrentProblem] = useState<Problem>();
 
+	const [roulette, setRoulette] = useState<Roulette<Problem> | undefined>();
+
+	useEffect(() => {
+		if (open) {
+			const tags = Array.from(filters.tags);
+			const difficulty = Array.from(filters.difficulty);
+			const premium = filters.premium;
+			api.getProblems({ tags, difficulty, premium }).then((problems) => {
+				setProblems(problems);
+				setRoulette(new Roulette(problems));
+			});
+		}
+	}, [open]);
+
+	useEffect(() => {
+		if (roulette && roulette.size !== 0) {
+			setCurrentProblem(roulette.problem);
+		}
+	}, [roulette]);
+
+	useEffect(() => {
+		console.log(currentProblem);
+	}, [currentProblem]);
 	return (
 		<>
-			<Modal show={open} onHide={() => toggleModalOpen()}>
+			<Modal className={`mx-auto`} size="lg" centered scrollable={true} show={open} onHide={() => toggleModalOpen()}>
 				<Modal.Header closeButton>
-					<Modal.Title>Modal heading</Modal.Title>
+					<Modal.Title>{currentProblem?.title}</Modal.Title>
 				</Modal.Header>
-				<Modal.Body></Modal.Body>
-				<Modal.Footer></Modal.Footer>
+				<Modal.Body>
+					{currentProblem && (
+						<>
+							<div
+								dangerouslySetInnerHTML={{
+									__html: currentProblem?.description || "<p>No Description</p>",
+								}}
+							></div>
+							{currentProblem?.hints.length && (
+								<div>
+									<p>
+										<strong>Hints:</strong>
+									</p>
+									<ul>
+										{currentProblem?.hints.map((hint) => {
+											return <li>{hint}</li>;
+										})}
+									</ul>
+								</div>
+							)}
+						</>
+					)}
+				</Modal.Body>
+				<Modal.Footer>{problems.length}</Modal.Footer>
 			</Modal>
 		</>
 	);
