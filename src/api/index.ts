b56f06af44feb98f10347389cc41c0db77b3810a
jared-ivory <26067982+Jared-Ivory@users.noreply.github.com>
2022-11-02 02:29:16 -0400

    Imported Problem type

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import LeetcodeRouletteAPI, { Tag } from "leetcode-roulette-api";
+import LeetcodeRouletteAPI, { Tag, Problem } from "leetcode-roulette-api";
 
-export { type Tag }
+export { type Tag, type Problem };
 export const api: LeetcodeRouletteAPI = new LeetcodeRouletteAPI("http://localhost:3001");
